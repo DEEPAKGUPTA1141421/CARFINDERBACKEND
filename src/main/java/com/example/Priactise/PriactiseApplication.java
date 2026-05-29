0c28ce54a35bcaa037601d5568999ed99038474f
DEEPAKGUPTA1141421 <deepak2198.be21@chitkara.edu.in>
2026-05-29 22:53:20 +0530

    backecd changes related tointent extraction and recommendation

---
+++ src/main/java/com/example/Priactise/PriactiseApplication.java
@@ -0,0 +1,16 @@
+package com.example.Priactise;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriactiseApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().loaad();
+		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+		SpringApplication.run(PriactiseApplication.class, args);
+	}
+
+}
